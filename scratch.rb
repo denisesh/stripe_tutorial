19269baa5ad28ebc7acc4431b8e8d5ed9727f381
denisesh <denisesh@gmail.com>
2015-01-31 17:40:56 -0800

    initial commit

---
+++ scratch.rb
@@ -0,0 +1,7 @@
+# taken out of partial prior to populate it in the controller
+# We will set this explictly to current_user
+
+  <div class="field">
+    <%= f.label :user_id %><br>
+    <%= f.text_field :user_id %>
+  </div>
